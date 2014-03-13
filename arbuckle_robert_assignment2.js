8599ab13b92cd29e077d9b7dcad927f936fd7f17
RobertArbuckle <robert.arbuckle@fullsail.edu>
2014-03-12 22:34:08 -0400

    math function added
    added the math function and started work on the string function. The
    project is almost complete.

--- arbuckle_robert_assignment2.js
+++ arbuckle_robert_assignment2.js
@@ -12,11 +12,9 @@ var totalReimbursement;
 var questComplete;
 // var reimbursementPerKillMultiplier; Unused due to redesign. 
 var rangedOrMelee;
+var sotrySummary;
 
-adventurerName = prompt("What is your name?");
-rangedOrMelee = prompt("Are you Ranged or Melee?");
-//console.log(adventurerName); used for testing purposes to verify inputs in the log
-//console.log(rangedOrMelee);
+
 function greetUser(rangedOrMelee, adventurerName) { //used to greet the user 
 
     if ((rangedOrMelee == "Ranged") && (adventurerName == "Drusilla")) {
@@ -36,7 +34,7 @@ function questCompletion(successfulQuest, adventurerName) { //this is the boolea
     var objectiveComplete;
     
     while ((objectiveNumber <= numberOfObjectives)&&successfulQuest) { //while loop that continues until all the objectives have been verified or the quest is marked incomplete. 
-        objectiveComplete=confirm("Did you complete onjective number: " + objectiveNumber);
+        objectiveComplete=confirm("Did you complete objective number: " + objectiveNumber);
         if (objectiveComplete) {
             console.log("Objective number: " + objectiveNumber + " complete!");
             objectiveNumber++
@@ -46,14 +44,44 @@ function questCompletion(successfulQuest, adventurerName) { //this is the boolea
             successfulQuest = false; //if a objective is not complete the quest was failed and the loop exits
         }
     }
-    return successfulQuest;
+    return successfulQuest; //returns boolean value
 
 }
 
+function payCalculator(totalkills, questComplete) { //this is the number function and calculates pay from the number of kills and adds the quest completion bonus if applicable
+    var payFromKills=0;
+    var totalPay=0;
+    for (iterator = 1; iterator <= totalkills; iterator++) { //basic for loop
+        payFromKills = payFromKills + REIMBURSEMENT_PER_KILL;
+        console.log("From kill number " + iterator + "you have made " + REIMBURSEMENT_PER_KILL + " gold pieces. Your total so far is: " + payFromKills + " gold pieces."); //outputs the pay for the kill + the running total
+    }
+    
+    if (questComplete) { //add the quest bonus if applicable
+        totalPay = payFromKills + REIMBURSEMENT_QUESTCOMPLETE;
+    }
+    else {
+        totalPay = payfromKills;
+    }
+    return totalPay; //returns the total amount the adventurer was paid. 
+
+}
+
+function summaryOfEvents(adventurerName, rangedOrMelee, questComplete, totalReimbursement) {
+    var completedString;
+    
+}
+
+adventurerName = prompt("What is your name?");
+rangedOrMelee = prompt("Are you Ranged or Melee?");
+//console.log(adventurerName); used for testing purposes to verify inputs in the log
+//console.log(rangedOrMelee);
 greetUser(rangedOrMelee, adventurerName); //calls the greetUser function - says hello to the user or guild leader. 
-questCompletion(confirm(adventurerName + " was your quest successful?"), adventurerName);
+questComplete = questCompletion(confirm(adventurerName + " was your quest successful?"), adventurerName);
+totalReimbursement=payCalculator(parseInt(prompt("How many monsters did you kill?")), questComplete);
 
 
+console.log("Was your quest successful?: " + questComplete);
+console.log("Your total pay was: " + totalReimbursement);
 
 
 
